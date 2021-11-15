a6513758d9a00022a03b8190a21c91a9c461ea95
christian-hilton <christian_hilton@apple.com>
2021-11-14 18:00:06 -0800

    A9 submission

---
+++ assignment9/Assets/LoadSceneOnInput.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneOnInput : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Submit") == 1) {
+			// Proceed from Title to Play Scene
+			if (SceneManager.GetActiveScene().name == "Title") {
+				SceneManager.LoadScene("Play");
+			}
+			// Proceed from GameOver to Title Scene
+			else {
+				SceneManager.LoadScene("Title");
+			}
+		}
+	}
+}
